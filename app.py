6921258ca1581e76cc78302dc0f20cf34ce13aef
kasarla rakesh <kasarlarakesh98@gmail.com>
2022-06-11 23:42:29 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+>py app.pyfrom flask import Flask
+import os
+
+
+app=Flask(__name__)
+
+@app.route('/',methods=['GET'])
+def index():
+    return "hello world"
+
+
+
+if __name__=="__main__":
+    app.run()
